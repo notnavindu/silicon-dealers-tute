9ab76b7d3e9575589fc82209a4fe628884601648
Layan Jayasinghe <layanjayasinghe2@gmail.com>
2021-06-22 23:08:09 +0530

    starter commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <div className='container'>Hello world</div>
+    </>
+  )
+}
+
+export default App
